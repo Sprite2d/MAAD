bb86c7cb0caf5800cc3139e923d42dd4ac57f104
Sprite2d <arushoduru6@gmail.com>
2025-01-11 16:35:37 +0530

    Update robotichand.ino

--- robotichand.ino
+++ robotichand.ino
@@ -12,10 +12,14 @@ void setup() {
 
 void loop() {
   Xval = analogRead(A0);
+  Yval = analogRead(A2);
   servopin = A1;
   servopin2 = A3;
   myservo.attach(servopin);
+  myservo2.attach(servopin2);
   int Mappedvalue = map(Xval,0,1023,0,180);
   int Mappedvaluey = map(Yval,0,1024,0,180);
   myservo.write(Mappedvalue);
+  myservo2.write(Mappedvaluey);
+  
 }

ac67c78debbd6e2658520b0fe6dfe0f19a233b8a
Sprite2d <arushoduru6@gmail.com>
2025-01-11 14:28:54 +0530

    Update robotichand.ino

--- robotichand.ino
+++ robotichand.ino
@@ -1,6 +1,9 @@
 #include <Servo.h>
 Servo myservo;
+Servo myservo2;
 int Xval;
+int Yval;
+int servopin2;
 int servopin;
 int positionval;
 void setup() {
@@ -10,7 +13,9 @@ void setup() {
 void loop() {
   Xval = analogRead(A0);
   servopin = A1;
+  servopin2 = A3;
   myservo.attach(servopin);
   int Mappedvalue = map(Xval,0,1023,0,180);
+  int Mappedvaluey = map(Yval,0,1024,0,180);
   myservo.write(Mappedvalue);
 }
